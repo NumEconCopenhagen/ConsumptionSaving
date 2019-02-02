26644b1be201785c35f68094f712a6bb3a389c2d
Jeppe Druedahl <gmf123@ku.dk>
2019-02-02 23:56:04 +0100

    New golden section search interface

--- consav/golden_section_search.py
+++ consav/golden_section_search.py
@@ -1,83 +1,68 @@
 # -*- coding: utf-8 -*-
-"""golden_section_search
+"""golden section search
 
 This module provides a Numba JIT compilled golden section search optimizer for a custom objective.
-
 """
 
 import math
+import numpy as np
 from numba import njit
 
-# constants
-inv_phi = (math.sqrt(5) - 1) / 2 # 1/phi                                                                                                                
-inv_phi_sq = (3 - math.sqrt(5)) / 2 # 1/phi^2                                                                                                                
-
-# create
-def create_optimizer(obj):
-    """ create golden section search optimizer for the function obj
+@njit
+def optimizer(a,b,tol,obj,*args):
+    """ optimizer
     
     Args:
 
-        obj (callable): objective function with *args (must be decorated with @njit)
+        a (double): minimum of starting bracket
+        b (double): maximum of starting bracket
+        tol (dobule): tolerance
+        obj: function to optimize over
+        *args: additional arguments to the objective function
 
     Returns:
 
         optimizer (callable): optimizer called as (a,b,tol,*args) where [a,b] is the starting bracket
     
     """
-
-    @njit
-    def golden_section_search(a,b,tol,*args):
-        """ optimizer
+    
+    inv_phi = (np.sqrt(5) - 1) / 2 # 1/phi                                                                                                                
+    inv_phi_sq = (3 - np.sqrt(5)) / 2 # 1/phi^2     
         
-        Args:
+    # a. distance
+    dist = b - a
+    if dist <= tol: 
+        return (a+b)/2
 
-            a (double): minimum of starting bracket
-            b (double): maximum of starting bracket
-            tol (tok): tolerance
-            *args: additional arguments to the objective function
+    # b. number of iterations
+    n = int(np.ceil(np.log(tol/dist)/np.log(inv_phi)))
 
-        Returns:
+    # c. potential new mid-points
+    c = a + inv_phi_sq * dist
+    d = a + inv_phi * dist
+    yc = obj(c,*args)
+    yd = obj(d,*args)
 
-            optimizer (callable): optimizer called as (a,b,tol,*args) where [a,b] is the starting bracket
-        
-        """
-            
-        # a. distance
-        dist = b - a
-        if dist <= tol: 
-            return (a+b)/2
-
-        # b. number of iterations
-        n = int(math.ceil(math.log(tol/dist)/math.log(inv_phi)))
-
-        # c. potential new mid-points
-        c = a + inv_phi_sq * dist
-        d = a + inv_phi * dist
-        yc = obj(c,*args)
-        yd = obj(d,*args)
-
-        # d. loop
-        for _ in range(n-1):
-            if yc < yd:
-                b = d
-                d = c
-                yd = yc
-                dist = inv_phi*dist
-                c = a + inv_phi_sq * dist
-                yc = obj(c,*args)
-            else:
-                a = c
-                c = d
-                yc = yd
-                dist = inv_phi*dist
-                d = a + inv_phi * dist
-                yd = obj(d,*args)
-
-        # e. return
+    # d. loop
+    for _ in range(n-1):
         if yc < yd:
-            return (a+d)/2
+            b = d
+            d = c
+            yd = yc
+            dist = inv_phi*dist
+            c = a + inv_phi_sq * dist
+            yc = obj(c,*args)
         else:
-            return (c+b)/2
+            a = c
+            c = d
+            yc = yd
+            dist = inv_phi*dist
+            d = a + inv_phi * dist
+            yd = obj(d,*args)
 
-    return golden_section_search
+    # e. return
+    if yc < yd:
+        return (a+d)/2
+    else:
+        return (c+b)/2
+    
