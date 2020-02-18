1f62b746a62582c54af8a80e5a598e4c8038a459
Jeppe Druedahl <gmf123@ku.dk>
2020-02-18 23:26:07 +0100

    Added elapsed

--- consav/misc.py
+++ consav/misc.py
@@ -4,12 +4,40 @@
 This module provides misc functions.
 """
 
+import time
 import math
 import numpy as np
 from scipy.stats import norm
 from numpy.linalg import svd
 from numba import njit
 
+def elapsed(t0):
+
+    secs = time.time()-t0
+    
+    days = secs//(60*60*24)
+    secs -= 60*60*24*days
+
+    hours = secs//(60*60)
+    secs -= 60*60*hours
+
+    mins = secs//(60)
+    secs -= 60*mins
+   
+    text = ''
+    if days > 0: text += f'{days} days '
+    if hours > 0: text += f'{hours} hours '
+    if mins > 0: text += f'{mins} mins '
+
+    if days > 0 or hours > 0:
+        pass
+    elif mins > 0:
+        text += f'{secs:.0f} secs '
+    else:
+        text = f'{secs:.1f} secs '
+
+    return text[:-1]
+    
 def nonlinspace(x_min, x_max, n, phi):
     """ like np.linspace. but with unequal spacing
 
