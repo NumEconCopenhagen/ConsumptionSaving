e6c27c7b18cd589a14aa0704bef7ccddfaff19f9
Jeppe druedahl <jeppe.druedahl@econ.ku.dk>
2024-09-27 10:00:20 +0200

    new version with interp_5d

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setuptools
 setuptools.setup(
     name='ConSav',
     url='https://github.com/NumEconCopenhagen/ConsumptionSaving',
-    download_url='https://github.com/NumEconCopenhagen/ConsumptionSaving/archive/refs/tags/v0.11.tar.gz',
-    version='0.11',
+    download_url='https://github.com/NumEconCopenhagen/ConsumptionSaving/archive/refs/tags/v0.12.tar.gz',
+    version='0.12',
     author='Jeppe Druedahl',
     packages=setuptools.find_packages(),
 )
